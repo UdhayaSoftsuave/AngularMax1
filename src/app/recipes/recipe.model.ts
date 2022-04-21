63151acedd5eb993b941c4f8eaed5e888d48b826
UdhayaSoftsuave <udhayakumar.softsuave@gmail.com>
2022-04-21 12:31:00 +0530

    Adding the ingredients to the recipe details

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,12 +1,16 @@
+import { ingredients } from "../Common/ingradient.model";
+
 export class RecepieModel {
     public name : string  ;
     public description: string  ;
     public imageUrl : string  ;
+    public ingredients : ingredients[];
 
-    constructor(name : string , description : string , imageUrl : string) {
+    constructor(name : string , description : string , imageUrl : string , ingredient : ingredients[] ) {
         this.name = name;
         this.description = description;
         this.imageUrl = imageUrl;
+        this.ingredients = ingredient;
     }
 
 }
