b5ee75f3b61f27c9a369650ccf05607650b91e12
UdhayaSoftsuave <udhayakumar.softsuave@gmail.com>
2022-04-25 13:04:52 +0530

    Craete the Observer

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 import { RecepieModel } from './recipe.model';
 
 @Component({
@@ -6,11 +7,25 @@ import { RecepieModel } from './recipe.model';
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.css']
 })
-export class RecipesComponent implements OnInit {
+export class RecipesComponent implements OnInit , OnDestroy {
+  private subcription! : Subscription;
   recipe!: RecepieModel;
   constructor() { }
 
   ngOnInit(): void {
+    const observe  = Observable.create((observer: any) => {
+      let count = 0 ;
+      setInterval(() => {
+        observer.next(count)
+        count++;
+      },500);
+  });
+  this.subcription = observe.subscribe((value: any) => {
+    // console.log(value);
+  })
+  }
+  ngOnDestroy(): void {
+    this.subcription.unsubscribe();
   }
   onselect(value : RecepieModel){
     this.recipe = value;
