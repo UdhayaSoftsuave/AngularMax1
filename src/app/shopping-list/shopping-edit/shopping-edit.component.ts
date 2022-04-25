4dd1094cf3d7380db270e1259580ada5a6cdf825
UdhayaSoftsuave <udhayakumar.softsuave@gmail.com>
2022-04-25 19:25:18 +0530

    Deleted the template Driven method for to  create the reative method

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormGroup, NgForm } from '@angular/forms';
 import { ingredients } from 'src/app/Common/ingradient.model';
 import { ShoppingService } from '../shopping.service';
 
@@ -9,36 +9,45 @@ import { ShoppingService } from '../shopping.service';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  
+  // @ViewChild('f', { static: true }) userdata!: NgForm;
+
+  // // @ViewChild('name', { static: true }) name!: ElementRef;
+  // // @ViewChild('amount', { static: true }) amount!: ElementRef;
+
+  // // ingredentName!: string;
+  // // ingredentAmount!: number;
+  // ingedient!: ingredients;
+
+  // constructor(private shoppingService : ShoppingService) { }
+
+  // ngOnInit(): void {
+  // }
+
+  // // onAdd(){
+  // //   this.ingredentName  = this.name.nativeElement.value;
+  // //   this.ingredentAmount  = this.amount.nativeElement.value;
+  // //   this.ingedient = new ingredients(this.ingredentName , this.ingredentAmount);
+  // //   this.shoppingService.addIngredients(this.ingedient);
+  // // }
+  // onsubmit(value : NgForm){
+  //   console.log(value);
+  //   if (value.valid === true) {
+  //     this.ingedient = new ingredients(value.value.userdata.name , value.value.userdata.amount);
+  //     this.shoppingService.addIngredients(this.ingedient);
+  //     this.userdata.reset();
+  //   } else {
+  //     alert("fill the form correctly")
+  //   } 
+  // }
+
   genders = ['Male' , 'Female'];
-  @ViewChild('f', { static: true }) userdata!: NgForm;
-
-  // @ViewChild('name', { static: true }) name!: ElementRef;
-  // @ViewChild('amount', { static: true }) amount!: ElementRef;
-
-  // ingredentName!: string;
-  // ingredentAmount!: number;
-  ingedient!: ingredients;
-
-  constructor(private shoppingService : ShoppingService) { }
+  signupForm !: FormGroup;
 
   ngOnInit(): void {
-  }
 
-  // onAdd(){
-  //   this.ingredentName  = this.name.nativeElement.value;
-  //   this.ingredentAmount  = this.amount.nativeElement.value;
-  //   this.ingedient = new ingredients(this.ingredentName , this.ingredentAmount);
-  //   this.shoppingService.addIngredients(this.ingedient);
-  // }
-  onsubmit(value : NgForm){
-    console.log(value);
-    if (value.valid === true) {
-      this.ingedient = new ingredients(value.value.userdata.name , value.value.userdata.amount);
-      this.shoppingService.addIngredients(this.ingedient);
-      this.userdata.reset();
-    } else {
-      alert("fill the form correctly")
-    } 
   }
+  
+
 
 }
