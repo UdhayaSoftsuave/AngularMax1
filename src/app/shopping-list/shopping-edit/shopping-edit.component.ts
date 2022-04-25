080584c248722a2c0cb9165eaaa5c257857a720f
UdhayaSoftsuave <udhayakumar.softsuave@gmail.com>
2022-04-25 17:46:45 +0530

    creating a form using in angular

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { ingredients } from 'src/app/Common/ingradient.model';
 import { ShoppingService } from '../shopping.service';
 
@@ -9,11 +10,11 @@ import { ShoppingService } from '../shopping.service';
 })
 export class ShoppingEditComponent implements OnInit {
 
-  @ViewChild('name', { static: true }) name!: ElementRef;
-  @ViewChild('amount', { static: true }) amount!: ElementRef;
+  // @ViewChild('name', { static: true }) name!: ElementRef;
+  // @ViewChild('amount', { static: true }) amount!: ElementRef;
 
-  ingredentName!: string;
-  ingredentAmount!: number;
+  // ingredentName!: string;
+  // ingredentAmount!: number;
   ingedient!: ingredients;
 
   constructor(private shoppingService : ShoppingService) { }
@@ -21,11 +22,20 @@ export class ShoppingEditComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onAdd(){
-    this.ingredentName  = this.name.nativeElement.value;
-    this.ingredentAmount  = this.amount.nativeElement.value;
-    this.ingedient = new ingredients(this.ingredentName , this.ingredentAmount);
-    this.shoppingService.addIngredients(this.ingedient);
+  // onAdd(){
+  //   this.ingredentName  = this.name.nativeElement.value;
+  //   this.ingredentAmount  = this.amount.nativeElement.value;
+  //   this.ingedient = new ingredients(this.ingredentName , this.ingredentAmount);
+  //   this.shoppingService.addIngredients(this.ingedient);
+  // }
+  onsubmit(value : NgForm){
+    console.log(value);
+    if (value.valid) {
+      this.ingedient = new ingredients(value.value.name , value.value.amount);
+      this.shoppingService.addIngredients(this.ingedient);
+    } else {
+      alert("fill the form correctly ")
+    } 
   }
 
 }
