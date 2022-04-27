f869c2b97bffa2594ba7431fd3425fcfa87aab02
UdhayaSoftsuave <udhayakumar.softsuave@gmail.com>
2022-04-27 16:49:09 +0530

    Creating Add Recipe-Component

---
+++ Angular-Notes.md
@@ -0,0 +1,5 @@
+property binding 
+================
+
+1) [foo]="true" -> typeof foo => 'boolean'  Note :-   [a] = "_";
+2) foo = "false" -> typeof foo => 'string'  Note :=   a = _ ;
--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+import { RecepieService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -9,17 +11,64 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class RecipeEditComponent implements OnInit {
   id! : number;
   isEdit = false;
-  constructor(private routes : ActivatedRoute) { }
+  product !: FormGroup;
+  constructor(private routes : ActivatedRoute , 
+    private recepieService : RecepieService) { }
+
+    
 
   ngOnInit(): void {
+    this.initForm();
     this.routes.params.subscribe(
       (param : Params) => {
         this.id = +param['id'];
         this.isEdit = param['id'] == null ? false : true;
-        console.log(this.isEdit);
+        this.initForm();
       }
     )
+  }
 
+  initForm(){
+    let ProductName = '';
+    let imageUrl = '';
+    let description = '';
+    let ingredients = new FormArray([]);
+    if (this.isEdit) {
+      const recepie = this.recepieService.getRecipiesById(this.id);
+      ProductName = recepie.name;
+      imageUrl = recepie.imageUrl;
+      description = recepie.description;
+      if (recepie.ingredients) {
+        for (const ing of recepie.ingredients) {
+          ingredients.push(new FormGroup({
+            'ingName' : new FormControl(ing.name ,Validators.required),
+            'ingAmount' : new FormControl(ing.amount ,[Validators.required , Validators.pattern(/^[1-9]+[0-9]*$/)])
+          }))
+        }
+      }
+    }
+    this.product = new FormGroup({
+      'name' : new FormControl(ProductName , Validators.required),
+      'imageUrl' : new FormControl(imageUrl ,Validators.required),
+      'description' : new FormControl(description ,Validators.required),
+      'ingredients' : ingredients
+    });
+  }
+
+  getIngredientsControl(){
+    const data = (this.product.controls["ingredients"] as FormArray).value;
+    return data;
+  }
+  onsubmit(){
+    console.log(this.product);
+  }
+  onAddIngredients(){
+    (<FormArray>this.product.get('ingredients')).push(
+      new FormGroup({
+        'ingName' : new FormControl(null ,Validators.required),
+        'ingAmount' : new FormControl(null ,[Validators.required ])
+      })
+      )
   }
 
 }
